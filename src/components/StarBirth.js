3ef93e78de44252b69c3a253a791abdd1150541f
Prestwick97 <prestwick97@gmail.com>
2020-05-18 10:30:13 -0700

    tudy how props work in react

--- src/components/StarBirth.js
+++ src/components/StarBirth.js
@@ -3,12 +3,12 @@ import "./styles.css";
 
 function StarBirth(){
   function handleStarCreation(event){
-    console.log(event.mass.value)
+    console.log(event.target.mass.value)
     event.preventDefault();
   }
   return(
     <React.Fragment>
-      <div class='createStar'>
+      <div className='createStar'>
       <h1>Testing</h1>
         <form id="massForm" onSubmit={handleStarCreation}>
             <input type='number' name='mass' placeholder='Mass (solar units)'/>

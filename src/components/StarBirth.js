7babd7f70b0ea97a95a28bd469c8f62fa670c559
Prestwick97 <prestwick97@gmail.com>
2020-05-18 10:18:48 -0700

    create handler function for star creation form

--- src/components/StarBirth.js
+++ src/components/StarBirth.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import "./styles.css";
 
-function StarBirth() {
+function StarBirth(){
+  function handleStarCreation(event){
+    console.log(event.mass.value)
+    event.preventDefault();
+  }
   return(
     <React.Fragment>
       <div class='createStar'>
       <h1>Testing</h1>
-        <form id="massForm">ß
+        <form id="massForm" onSubmit={handleStarCreation}>
             <input type='number' name='mass' placeholder='Mass (solar units)'/>
             <button>{"Create your star"}</button>
         </form>
